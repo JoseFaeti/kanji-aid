d268c40cba3d35f16cf5bb0088fcaf1b6db6ed36
itto <josefaeti@gmail.com>
2015-12-14 12:30:57 +0100

    Implemented sets using Data.Set to replace slow operations on lists

--- test.hs
+++ test.hs
@@ -9,25 +9,27 @@
 
 import Data.List
 import Data.List.Split
+import Data.Maybe
+--import qualified Data.HashMap.Lazy as HashMap
+import qualified Data.Set as Set
 import System.Console.ANSI
 
 main :: IO ()
 main = do
 
-    -- Starts to run crappily when set to tens of thousands
-    let word_limit = 1000
+    let word_limit = 100000
     
     -- Load the jouyou kanji list
     jouyou <- readFile "jouyou.txt"
-    let jouyou_list = lines jouyou
+    let jouyou_set = Set.fromList $ lines jouyou
     
     -- Load the known kanji list
     known <- readFile "known.txt"
-    let known_kanji_list = [[head x] | x <- lines known, length x > 1]
+    let known_kanji_set = Set.fromList [[head x] | x <- lines known, length x > 1]
 
     -- Load the known kana (and other typography symbols) list
     known_kana <- readFile "known_kana.txt"
-    let known_kana_list = [c | line <- lines known_kana, c <- words line]
+    let known_kana_set = Set.fromList [c | line <- lines known_kana, c <- words line]
 
     -- Read the list of the most frequent words
     frequent <- readFile "most frequent words.txt"
@@ -38,22 +40,22 @@ main = do
     let max_words = if word_limit < frequent_list_length then word_limit else frequent_list_length
     let frequent_list = take max_words $ frequent_list_lines
 
-    let known_list                    = known_kanji_list ++ known_kana_list
-    let known_jouyou_list             = [k | k <- known_kanji_list, elem k jouyou_list]
-    let kanji_seen_duplicates_list    = [kanji | word <- frequent_list, kanji <- word, not $ elem [kanji] known_kana_list]
-    let kanji_seen_list               = nub kanji_seen_duplicates_list
-    let kanji_seen_list_length        = length kanji_seen_list
-    let unknown_kanji_duplicates_list = [kanji | kanji <- kanji_seen_duplicates_list, not $ elem [kanji] known_list]
+    let known_set                     = Set.union known_kanji_set known_kana_set
+    let known_jouyou_set              = Set.filter (\k -> Set.member k jouyou_set) known_kanji_set
+    let kanji_seen_duplicates_list    = [kanji | word <- frequent_list, kanji <- word, not $ Set.member [kanji] known_kana_set]
+    let kanji_seen_set                = Set.fromList $ kanji_seen_duplicates_list
+    let total_kanji_seen              = Set.size kanji_seen_set
+    let unknown_kanji_duplicates_list = [kanji | kanji <- kanji_seen_duplicates_list, not $ Set.member [kanji] known_set]
 
-    -- check how often each kanji is present in words
+    -- check how often each kanji is present in words (might be better suited for a HashMap)
     let unknown_kanji_frequency_list  = reverse $ sort $ map (\l -> (length l, head l)) $ group $ sort unknown_kanji_duplicates_list
 
-    let unknown_kanji_list            = nub unknown_kanji_duplicates_list
-    let unreadable_word_list          = [w | w <- frequent_list, k <- w, not $ elem [k] known_list]
+    let unknown_kanji_set             = Set.fromList unknown_kanji_duplicates_list
+    let unreadable_word_list          = [w | w <- frequent_list, k <- w, not $ Set.member [k] known_set]
     let total_readable_words          = max_words - (length unreadable_word_list)
-    let total_kanji_recognized        = kanji_seen_list_length - length unknown_kanji_list
-    let recognized_percentage         = (100 * total_kanji_recognized) `div` kanji_seen_list_length
-    let jouyou_known_percentage       = (100 * length known_jouyou_list) `div` length jouyou_list
+    let total_kanji_recognized        = total_kanji_seen - Set.size unknown_kanji_set
+    let recognized_percentage         = (100 * total_kanji_recognized) `div` total_kanji_seen
+    let jouyou_known_percentage       = (100 * Set.size known_jouyou_set) `div` Set.size jouyou_set
     let readable_words_percentage     = (100 * total_readable_words) `div` max_words
 
     -- Output data (unknown, non-jouyou kanji marked in red)
@@ -61,19 +63,19 @@ main = do
 
     putStrLn "Unknown kanji:"
     mapM_ (\k ->
-        if elem k jouyou_list
+        if Set.member [k] jouyou_set
         then do
             setSGR [SetColor Foreground Vivid White]
-            putStr k
+            putStr [k]
         else do 
             setSGR [SetColor Foreground Vivid Red]
-            putStr k) [[k] | k <- unknown_kanji_list]
+            putStr [k]) $ Set.toList unknown_kanji_set
 
     setSGR [SetColor Foreground Vivid White]
 
     putStrLn   ""
-    putStrLn $ "Kanji known:           " ++ show (length known_kanji_list) ++ " (" ++ show jouyou_known_percentage ++ "% of jouyou kanji)"
-    putStrLn $ "Kanji recognized:      " ++ show total_kanji_recognized ++ "/" ++ show kanji_seen_list_length ++ " (" ++ show recognized_percentage ++ "%)"
+    putStrLn $ "Kanji known:           " ++ show (Set.size known_kanji_set) ++ " (" ++ show jouyou_known_percentage ++ "% of jouyou kanji)"
+    putStrLn $ "Kanji recognized:      " ++ show total_kanji_recognized ++ "/" ++ show total_kanji_seen ++ " (" ++ show recognized_percentage ++ "%)"
     putStrLn $ "Readable words:        " ++ show total_readable_words ++ "/" ++ show max_words ++ " (" ++ show readable_words_percentage ++ "%)"
 
     putStrLn   "Best kanji to learn next:"
@@ -82,3 +84,7 @@ main = do
                                     [snd $ unknown_kanji_frequency_list !! n] ++ " seen in " ++ show (fst $ unknown_kanji_frequency_list !! n) ++ " words    ") [t,t+10..t+20]) [0..10]
 
     putStr   $ "Most common unreadable words:\n" ++ unlines (take 10 unreadable_word_list)
+
+    --let m = HashMap.fromList [("foo", "bar")]
+
+    --putStrLn $ show $ fromJust $ HashMap.lookup "foo" m
